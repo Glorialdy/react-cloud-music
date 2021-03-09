452c57340f4691a22a4fa046cbc12579e5703f8f
yang <yang@dell-pc.localdomain>
2021-03-09 12:22:32 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { GlobalStyle } from './style';
 import {renderRoutes} from "react-router-config"; //读取路由配置，转换为Route标签
 import routes from "./routes/index";
 import { HashRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import store from './store/index'
 
 function App() {
   return (
-    <Provider>
+    <Provider store = {store}>
       <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      { renderRoutes (routes) }
-    </HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        { renderRoutes (routes) }
+      </HashRouter>
     </Provider>
   );
 }
